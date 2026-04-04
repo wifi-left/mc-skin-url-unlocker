de62a7042d580765d1899b60be9356cd574cda47
wifi-left <ysy1679682327@outlook.com>
2026-04-04 22:22:55 +0800

    1.0.2

---
+++ src/main/java/io/wifi/skinunlocker/SkinUnlockerClient.java
@@ -0,0 +1,16 @@
+package io.wifi.skinunlocker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SkinUnlockerClient implements ModInitializer {
+
+    public static Logger LOGGER = LoggerFactory.getLogger("skinunlocker");
+
+
+    @Override
+    public void onInitialize() {
+    }
+}
--- src/main/java/io/wifi/skinunlocker/mixin/AllowUrlMixin.java
+++ src/main/java/io/wifi/skinunlocker/mixin/AllowUrlMixin.java
@@ -1,4 +1,4 @@
-package io.wifi.skinunlocker.client.mixin;
+package io.wifi.skinunlocker.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
